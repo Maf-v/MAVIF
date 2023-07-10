32391adcf94a4c3483da12b156469d933e4d76e7
MAF <para_siempre_boca@hotmail.com>
2023-07-10 11:08:22 -0300

    Test professionalCard LWC y ajuste de valoracion general a un decimal

--- force-app/main/default/lwc/professionalCard/professionalCard.js
+++ force-app/main/default/lwc/professionalCard/professionalCard.js
@@ -1,39 +1,40 @@
-import { LightningElement, api, wire } from 'lwc';
-import getReviewsByProfessionalName from '@salesforce/apex/ProfessionalsController.getReviewsByProfessionalName';
+import { LightningElement, api, wire } from "lwc";
+import getReviewsByProfessionalName from "@salesforce/apex/ProfessionalsController.getReviewsByProfessionalName";
 
 export default class ProfessionalsList extends LightningElement {
-    @api professionalData;
-    listReviews;
-    averageRating = 0;
-    cantReviews = 0;
-    error;
+  @api professionalData;
+  listReviews;
+  averageRating = 0;
+  cantReviews = 0;
+  error;
 
-    activeSectionMessage = '';
+  activeSectionMessage = "";
 
-    handleToggleSection(event) {
-        this.activeSectionMessage =
-            'Open section name:  ' + event.detail.openSections;
+  handleToggleSection(event) {
+    this.activeSectionMessage =
+      "Open section name:  " + event.detail.openSections;
+  }
+
+  @wire(getReviewsByProfessionalName, {
+    professionalName: "$professionalData.Name"
+  })
+  wiredReviews(results) {
+    if (results.data) {
+      this.listReviews = results.data;
+      this.averageRating = this.getAverageRating(results.data);
+      this.cantReviews = results.data.length;
+    } else if (results.error) {
+      this.error = results.error;
     }
+  }
 
-    @wire(getReviewsByProfessionalName, { professionalName: '$professionalData.Name'})
-    wiredReviews(results) { 
-        if (results.data) {
-            this.listReviews = results.data;
-            this.averageRating = this.getAverageRating(results.data);
-            this.cantReviews = results.data.length;
-        } else if (results.error) {
-            this.error = results.error;
-        }
+  getAverageRating(listReviews) {
+    let ratings = [];
+    for (let review of listReviews) {
+      ratings.push(parseInt(review.Valoracion__c, 10));
     }
-
-    getAverageRating(listReviews) {
-        let ratings = [];
-        for (let review of listReviews) {
-            ratings.push(parseInt(review.Valoracion__c));
-        }
-        let sum = ratings.reduce((acum, current) => acum + current);
-        let average = parseFloat(sum / ratings.length).toFixed(2);
-        return average;
-    }
-
+    let sum = ratings.reduce((acum, current) => acum + current);
+    let average = parseFloat(sum / ratings.length).toFixed(1);
+    return average;
+  }
 }
